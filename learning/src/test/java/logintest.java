a9f664dfdff4d20dff2c208386df85bbab172a07
Abeesh S <abeeshsengottuvel12@gmail.com>
2026-03-30 23:58:46 +0530

    Add assertion and maximize browser in test
    Update logintest.java to import Arrays and Playwright assertions, launch Chromium with --start-maximized and headless=false, navigate to the-internet site, assert the landing image text, add a short wait and properly close page and browser. Remove generated build artifacts and test reports (target jar, surefire XML/txt) and update compiled test class.

--- learning/src/test/java/logintest.java
+++ learning/src/test/java/logintest.java
@@ -1,20 +1,32 @@
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 public class logintest {
 
     @Test
     public void login() throws InterruptedException
     {
         Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+        .setArgs(Arrays.asList("--start-maximized")) // Note the 's' in Arrays
+        .setHeadless(false));
         
         Page page = browser.newPage();
-        page.navigate("https://www.google.com/");
+        page.navigate("https://the-internet.herokuapp.com/");
         
+        assertThat(page.getByAltText("Welcome to the-internet")).hasText("Welcome to the-internet");
+
+        Thread.sleep(10000);
+
+        page.close();
+        browser.close();
 
     }
 }
